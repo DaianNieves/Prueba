dd67fe30d7de469e4cf1d07ce9cc6ffff5fe4c2e
DaianNieves <daiannieves10@gmail.com>
2024-09-02 07:52:29 -0600

    Added Array functions and use of an object class

--- app/src/main/java/com/example/prueba/clases/Variables.kt
+++ app/src/main/java/com/example/prueba/clases/Variables.kt
@@ -18,12 +18,27 @@ package com.example.prueba.clases
         var isGreater:Boolean = false;
 
         //Array
-        val names = arrayOf("Daián", "Nataly", "Eduardo", "Gabriela");
+        val names = arrayOf("Daian", "Nataly", "Eduardo", "Gabriela");
 
         println(edad);
         println("Welcome $name, to your first Kotlin project");
         println(add())
         println(product(x=10,y=92))
+        println(printArray(names))
+
+        println(names.joinToString())
+
+        val numbers = arrayOf(1,2,3,4,5,6,7,8,9);
+        isEven(numbers)
+
+        println (getDay(1))
+
+        val person = Person(name = "Andrea", age = 22)
+
+        println(person.name)
+        println(person.age)
+
+        person.displayInformation()
        }
 
 fun add():Int {
@@ -34,8 +49,43 @@ fun add():Int {
 }
 
 fun product(x:Int, y:Int):Int {
-
     return (x+y);
+}
 
+fun printArray(names:Array<String>){
+    for (name in names){
+        print("Hello $name")
+    }
+}
+
+fun isEven(numbers:Array<Int>){
+    for (number in numbers){
+        if (number%2 == 0){
+            println("El numero $number es Par")
+        }else{
+            println("El numero $number no es Par")
+        }
+    }
+}
+
+fun getDay(day:Int):String{
+    var name = ""
+    when(day){
+        1 -> name = "Monday";
+        2 -> name = "Tuesday";
+        3 -> name = "Wednesday";
+        4 -> name = "Thursday";
+        5 -> name = "Friday";
+        6 -> name = "Saturday";
+        7 -> name = "Sunday";
+        else -> name = "Incorrect value";
+    }
+    return name
+}
+
+class Person (val name:String, val age:Int){
+    fun displayInformation(){
+        println("Name: $name, Age: $age")
+    }
 }
 //}
