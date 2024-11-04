b6e55008b23ba7e69e010d5f9bc3d0b5805e57a1
DaianNieves <daiannieves10@gmail.com>
2024-11-04 00:23:10 -0600

    Add :sparkles: Update on Main

--- app/src/main/java/com/example/project1/MainActivity.kt
+++ app/src/main/java/com/example/project1/MainActivity.kt
@@ -28,7 +28,6 @@ import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -48,13 +47,11 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import com.example.project1.ui.theme.Project1Theme
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.project1.ui.navigation.NavManager
 import com.example.project1.ui.screens.ComponentsScreen
 import com.example.project1.ui.screens.HomeScreen
 import com.example.project1.ui.screens.LocationScreen
@@ -68,10 +65,288 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ComposeMultiScreenApp()
+            /*
+            /*Project1Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Daián",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }*/
+
+            //layouts
+
+           /*Column{
+                Text(text = "First Row")
+                Text(text = "Second Row")
+                Text(text = "Third Row")
+                Row{
+                    Text(text = "TEXT 1")
+                    Text(text = "TEXT 2")
+                    Text(text = "TEXT 3")
+                    Text(text = "TEXT 4")
+                }
+                Box{
+                    Text(text = "Label 1")
+                    Text(text = "Label 2")
+                }
+                Greeting(name = "World")
+            }*/
+
+            Column(
+                modifier = Modifier.fillMaxSize()
+                    .fillMaxSize()
+                    .verticalScroll(rememberScrollState()),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = androidx.compose.ui.Alignment.CenterHorizontally
+            ){
+                CustomText()
+                PictureComposable()
+                Content1()
+                Content2()
+                BoxExample()
+                BoxExample2()
+
+            }*/
+
         }
     }
 }
+/*
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
 
+//@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Project1Theme {
+        Greeting("Android")
+    }
+}
+
+//@Preview(showBackground = true)
+@Composable
+fun ModifierExample(){
+    Column(
+        modifier = Modifier
+            .padding(24.dp)
+    ) {
+        Text(text = "Hello World")
+    }
+}
+
+//@Preview(showBackground = true)
+@Composable
+fun ModifierExample2(){
+    Column(
+        modifier = Modifier
+            .padding(24.dp)
+            .fillMaxWidth()
+            .clickable(onClick = { clickAction() })
+    ) {
+        Text(text = "Hello World")
+    }
+}
+
+//@Preview(showBackground = true)
+@Composable
+fun ModifierExample3(){
+    Column(
+        modifier = Modifier
+            .fillMaxHeight()
+            .padding(16.dp)
+            .background(Color.Cyan)
+            .border(width = 2.dp, color = Color.Green)
+            .width(200.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Text(text = "Item 1")
+        Text(text = "Item 2")
+        Text(text = "Item 3")
+        Text(text = "Item 4")
+        Text(text = "Item 5")
+    }
+}
+
+//@Preview(showBackground = true)
+@Composable
+fun CustomText(){
+    Column{
+        Text(
+            stringResource(R.string.hello_world_text),
+            color = colorResource(R.color.purple_700),
+            fontSize = 28.sp,
+            fontStyle = FontStyle.Italic,
+            fontWeight = FontWeight.ExtraBold
+        )
+        val gradientColors = listOf(Color.Cyan, Color.Blue, Color.Red)
+        Text(
+            stringResource(R.string.hello_world_text),
+            style = TextStyle(brush = Brush.linearGradient(colors = gradientColors))
+        )
+    }
+}
+
+//@Preview(showBackground = true)
+@Composable
+fun PictureComposable(){
+    Column (
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.Black)
+    ){
+        Image(
+            modifier = Modifier
+                .fillMaxWidth(),
+            painter = painterResource(R.drawable.android_logo),
+            contentDescription = "Logo Android",
+            contentScale = ContentScale.Crop
+        )
+    }
+}
+
+
+//@Preview(showBackground = true)
+@Composable
+fun Content1(){
+    Card(
+        modifier = Modifier
+            .background(Color.LightGray)
+            .fillMaxWidth()
+            .padding(5.dp)
+    ){
+        Text (text = "This is a Title",
+        fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(10.dp)
+        )
+        Image(
+            modifier = Modifier
+                .fillMaxWidth(),
+            painter = painterResource(id = R.drawable.android_logo),
+            contentDescription = "Android Logo",
+            contentScale = ContentScale.Crop
+        )
+        Text(
+            stringResource(R.string.text_card),
+            textAlign = TextAlign.Justify,
+            lineHeight = 18.sp,
+            modifier = Modifier
+            .padding(10.dp)
+        )
+    }
+}
+
+//@Preview(showBackground = true)
+@Composable
+fun Content2(){
+    Card(
+        modifier = Modifier
+            .background(Color.LightGray)
+            .fillMaxWidth()
+            .padding(5.dp)
+    ){
+        Row {
+            Image(
+                modifier = Modifier
+                    .width(150.dp)
+                    .height(150.dp),
+                    //.fillMaxWidth()
+                painter = painterResource(id = R.drawable.android_logo),
+                contentDescription = "Android Logo",
+                contentScale = ContentScale.Crop
+            )
+            Column {
+                Text (text = "This is a Title",
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .padding(10.dp)
+                )
+                Text(
+                    stringResource(R.string.text_card),
+                    textAlign = TextAlign.Justify,
+                    lineHeight = 18.sp,
+                    maxLines = 4,
+                    modifier = Modifier
+                        .padding(10.dp)
+                )
+            }
+
+
+        }
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BoxExample(){
+    Box(
+        modifier = Modifier
+            .background(Color.DarkGray)
+            .fillMaxWidth()
+            .padding(5.dp)
+    ){
+        Image(painterResource(R.drawable.android_logo),
+            contentDescription = "Android Logo",
+            contentScale = ContentScale.FillBounds
+        )
+
+        Row(
+            modifier = Modifier
+                .background(Color.Red)
+                .fillMaxWidth()
+                .padding(0.dp,0.dp),
+            horizontalArrangement = Arrangement.Center
+
+        ){
+            Icon(
+                Icons.Filled.AccountBox,
+                contentDescription = "Icon Account"
+            )
+            Text(text = "Text")
+        }
+
+
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BoxExample2(){
+    Box(
+        modifier = Modifier
+            .background(Color.Magenta)
+            .padding(5.dp)
+            .size(250.dp)
+    ){
+        Text(text = "TopStart", Modifier.align(Alignment.TopStart))
+        Text(text = "TopEnd", Modifier.align(Alignment.TopEnd))
+
+        Text(text = "CenterStart", Modifier.align(Alignment.CenterStart))
+        Text(text = "Center", Modifier.align(Alignment.Center))
+        Text(text = "CenterEnd", Modifier.align(Alignment.CenterEnd))
+
+        Text(text = "BottomStart", Modifier.align(Alignment.BottomStart))
+        Text(text = "BottomEnd", Modifier.align(Alignment.BottomEnd))
+
+    }
+}
+
+fun clickAction(){
+    println("Column Clicked")
+}
+*/
 
 @Composable
 fun ComposeMultiScreenApp() {
@@ -88,7 +363,6 @@ fun SetupNavGraph(navController: NavHostController) {
         composable("home") { HomeScreen(navController) }
         composable("componentes") { ComponentsScreen(navController) }
         composable("Location"){ LocationScreen((navController)) }
-
     }
 }
 
