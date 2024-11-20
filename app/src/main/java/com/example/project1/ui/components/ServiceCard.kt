b26c4fbdfcec0994e96fcda77f4b6eab5f279f93
DaianNieves <daiannieves10@gmail.com>
2024-11-20 07:55:18 -0600

    Add :sparkles: ServiceDetailCard and some modifications on ServiceCard

--- app/src/main/java/com/example/project1/ui/components/ServiceCard.kt
+++ app/src/main/java/com/example/project1/ui/components/ServiceCard.kt
@@ -10,81 +10,88 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import coil.compose.AsyncImage
 import com.example.project1.R
 
-@Preview(showBackground = true)
 @Composable
-fun ServiceCard(navController: NavHostController) {
-    Column(
+fun ServiceCard(
+    id:Int,
+    name: String,
+    username:String,
+    imageURL: String?,
+    onButtonClick:()->Unit) {
+
+    Card(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Top,
-        horizontalAlignment = Alignment.CenterHorizontally
+            .padding(3.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.Black,
+            contentColor = Color.White
+        )
     ) {
-        Spacer(modifier = Modifier.height(60.dp))
-
-        // Card que contiene el Row
-        Card(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp),
-            shape = RoundedCornerShape(8.dp), // Bordes redondeados
-            elevation = CardDefaults.elevatedCardElevation(4.dp), // Sombra del Card
-            colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surface)
-        ) {
+        Row() {
+            AsyncImage(
+                modifier = Modifier
+                    .width(80.dp)
+                    .height(88.dp)
+                    .padding(18.dp),
+                model = imageURL,
+                error = painterResource(R.drawable.android_logo),
+                contentDescription = "Service logo ",
+                contentScale = ContentScale.FillBounds
+            )
+            Column() {
+                Text(
+                    text = name,
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(2.dp, 8.dp, 0.dp, 0.dp)
+                )
+                Text(
+                    text = name,
+                    fontSize = 18.sp,
+                    modifier = Modifier.padding(4.dp, 0.dp)
+                )
+            }
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(16.dp),
-                horizontalArrangement = Arrangement.SpaceBetween,
+                    .height(80.dp),
+                horizontalArrangement = Arrangement.End,
                 verticalAlignment = Alignment.CenterVertically
-            ) {
-                Image(
-                    painter = painterResource(id = R.drawable.android_logo),
-                    contentDescription = "Sample Image",
-                    modifier = Modifier.size(64.dp)
-                )
-
-                Column(
+            ){
+                IconButton(
                     modifier = Modifier
-                        .weight(1f)
-                        .padding(horizontal = 16.dp)
+                        .padding(20.dp,0.dp,0.dp,0.dp),
+                    onClick = {onButtonClick()}
                 ) {
-                    Text(
-                        text = "Texto Superior",
-                        fontSize = 14.sp,
-                        fontWeight = FontWeight.Bold
-                    )
-                    Text(
-                        text = "Texto Inferior",
-                        fontSize = 14.sp,
-                        color = MaterialTheme.colorScheme.onBackground
-                    )
-                }
 
-                Button(
-                    onClick = { /* Acción del botón */ },
-                    modifier = Modifier.size(width = 100.dp, height = 40.dp)
-                ) {
-                    Text(
-                        text = "Botón",
-                        fontSize = 14.sp
+                    Icon(
+                        imageVector = Icons.Filled.MoreVert,
+                        contentDescription = "Service Details"
                     )
                 }
             }
